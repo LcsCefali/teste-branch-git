09f6a235888456962ce8496aa697894e1c493a9a
Lucas Cefali <lucas.cefali@gmail.com>
2020-08-19 10:17:30 -0300

    changes in test on share function

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ console.log("Hello World");
 
 console.log("> Branch: Master PLUS");
 
-function share(){
-    console.log("Compartilhe seu dia com quem você ama");
+function share() {
+  console.log(
+    "Compartilhe seu dia com quem você ama, mas só com quem ama muito!"
+  );
 }
 console.log("> Branch: Branch do Lucas");
 
